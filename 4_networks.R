a3bdc7cb146d9a04247245cf7e5f2e217cf6d2be
Lauren Ponisio <lponisio@gmail.com>
2022-02-28 14:39:24 -0800

    considered adding netowrks of articles but perhaps too different from the rest

---
+++ 4_networks.R
@@ -0,0 +1,8 @@
+## setwd("~/Dropbox (University of Oregon)/")
+setwd("/Volumes/bombus/Dropbox (University of Oregon)")
+## setwd("\Dropbox (University of Oregon)")
+
+setwd("network-bias-saved")
+
+
+library(ggplot2)
