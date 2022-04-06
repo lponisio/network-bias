4756841536c320a079c8132fa9d4357234972ee9
lponisio <lponisio@gmail.com>
2022-04-06 14:36:12 -0700

    re added raw data to avoid issues with brazil-US excel conversions

--- 1_geo_eco_regions.R
+++ 1_geo_eco_regions.R
@@ -10,7 +10,7 @@ library(dplyr)
 setwd("~/Dropbox (University of Oregon)/")
 ## setwd("/Volumes/bombus/Dropbox (University of Oregon)")
 ## setwd("\Dropbox (University of Oregon)")
-setwd("C:/Users/emanu/Dropbox (University of Oregon)")
+## setwd("C:/Users/emanu/Dropbox (University of Oregon)")
 
 setwd("network-bias-saved")
 
