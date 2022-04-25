ac3709fdb767debdbf74a2358dc87b90e43d7d90
Emanuelle Brito <emanuelle.biologa@gmail.com>
2022-04-25 14:07:44 -0700

    refreshing data

--- 2_multinom_tests.R
+++ 2_multinom_tests.R
@@ -2,7 +2,7 @@ rm(list=ls())
 setwd("~/Dropbox (University of Oregon)/")
 ## setwd("/Volumes/bombus/Dropbox (University of Oregon)")
 ## setwd("\Dropbox (University of Oregon)")
-## setwd("C:/Users/emanu/Dropbox (University of Oregon)")
+setwd("C:/Users/emanu/Dropbox (University of Oregon)")
 
 setwd("network-bias-saved")
 
