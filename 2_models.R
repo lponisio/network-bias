eb2d1294664ef2360d542eb5631281140e8be35d
rosemcd <rmcdonal@uoregon.edu>
2025-04-11 08:55:07 -0700

    updated models

--- 2_models.R
+++ 2_models.R
@@ -18,6 +18,16 @@ source("network-bias/src/initalize_models.R")
 # variables. We included an interaction with each of these variables and continent 
 # to allow their slopes to vary by continent.  
 ## ***********************************************
+webs_complete <- webs_complete[webs_complete$Continent != "Oceania",]
+
+webs_complete$Continent <- factor(webs_complete$Continent,
+                                 levels=c("Northern America",
+                                          "Southern America",
+                                          "Africa",
+                                          "Europe",
+                                          "Asia"))
+
+
 webs_country <- webs_complete %>%
   distinct(ISO3, .keep_all = TRUE) %>%
   filter(!is.na(Continent) & 
@@ -25,31 +35,18 @@ webs_country <- webs_complete %>%
            !is.na(PropGDP_median) & 
            !is.na(CL_Species))
 
-webs_country <- webs_country[webs_country$Continent != "Oceania",]
-
-webs_country$Continent <- factor(webs_country$Continent,
-                                     levels=c("Northern America",
-                                              "Southern America",
-                                              "Africa",
-                                              "Europe",
-                                              "Asia"))
 
 # Ensure the dataset contains the required transformed variables
 webs_country$log_AREA <- scale(log(webs_country$AREA))
 webs_country$log_PropGDP_median <- scale(log(webs_country$PropGDP_median))
 webs_country$log_CL_Species <- scale(log(webs_country$CL_Species))
 
-
-M1 <- MASS::glm.nb(Total_webs_by_country ~ Continent +
-                log_AREA +
-                log_PropGDP_median +
-                log_CL_Species,
-              #family = nbinom2,
-             data = webs_country)
-
-summary(M1)
-check_model(M1)
-
+M1 <- glm.nb(Total_webs_by_country ~ Continent +
+                     log_AREA +
+                     log_PropGDP_median +
+                     log_CL_Species,
+                   #family = nbinom2,
+                   data = webs_country)
 
 
 boot_fun <- function(model, data) {
@@ -135,19 +132,62 @@ webs_reuse <- webs_complete %>%
   filter(!is.na(years_since_pub),
          years_since_pub<90)
 
+webs_reuse$years_since_pub
 
 library(glmmTMB)
 # Model with variable dispersion
-webs_reuse_mod <- glmmTMB(Use_Frequency ~ ISO3 + years_since_pub,
-                          data = webs_reuse, ziformula = ~ 1, 
+webs_reuse_mod <- glmmTMB(Use_Frequency-1 ~ Continent * years_since_pub,
+                          data = webs_reuse, 
                           family = nbinom2
                          )
-
 summary(webs_reuse_mod)
-
 performance::check_model(webs_reuse_mod)
 
-years_since_pub
+
+#webs_reuse$Use_Frequency <-(webs_reuse$Use_Frequency-1)
+
+
+library(MASS)
+M1_nb <- glm.nb(Use_Frequency ~ Continent *years_since_pub, data = webs_reuse)
+summary(M1_nb)
+performance::check_model(M1_nb)
+
+
+boot_fun <- function(model, data) {
+  # Extract fitted values (mu) and dispersion parameter (theta)
+  mu <- fitted(model)  # Mean of the negative binomial distribution
+  theta <- model$theta  # Dispersion parameter
+  
+  # Simulate new response values using the negative binomial distribution
+  sim_response <- rnbinom(n = length(mu), size = theta, mu = mu)
+  
+  # Replace the response column with the simulated data
+  data$Use_Frequency <- sim_response
+  
+  # Fit the model to the simulated data
+  sim_model <- update(model, data = data)
+  
+  # Return the coefficients from the simulated model
+  return(coef(sim_model))
+}
+
+n_iter <- 1000  # Number of bootstrap iterations
+boot_results <- replicate(n_iter, boot_fun(M1_nb, webs_reuse))
+
+
+
+
+
+boot_results_df <- as.data.frame(t(boot_results))
+colnames(boot_results_df) <- names(coef(M1_nb))  # Assign coefficient names
+boot_ci <- apply(boot_results_df, 2, function(x) quantile(x, probs = c(0.025, 0.975)))  # 95% CI
+boot_ci
+
+
+
+#check hurdle and boot strap them both
+#double check if the re-use is updated 
+#its ok to exclude, and why we exxluded 
 
 ## ***********************************************
 #gonna put this in a function/source file later but I am trying to tidy up my 
@@ -165,7 +205,7 @@ table_country <- format_glm_table(
 )
 
 table_reuse <- format_glm_table(
-  webs_reuse_mod,
+  M1_nb,
   caption = "webs_reuse_mod"
 )
 
