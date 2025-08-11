879b27fd94afa052c9763dfb3444eec2f7dcd23d
Emanuelle Brito <emanuelle.biologa@gmail.com>
2025-08-11 16:36:11 -0300

    Required packages

---
+++ src/initialize_packages.R
@@ -0,0 +1,29 @@
+# =========================================================
+# Required Packages
+# - Mapping and cartography 
+# - Visualization 
+# - Modeling/statistics
+# - Tables/reports
+# - Data handling/utilities
+# - Latex
+# =========================================================
+
+library(cartogram)      # create cartograms (rescaled maps)
+library(countrycode)    # convert country names and codes
+library(cowplot)        # combine ggplot2 plots
+library(DHARMa)         # residual diagnostics for GLM/GLMM
+library(dplyr)          # data manipulation (also in tidyverse)
+library(ggplot2)        # data visualization (also in tidyverse)
+library(grid)           # low-level grid graphics
+library(kableExtra)     # create HTML/PDF tables
+library(lme4)           # linear and generalized linear mixed models
+library(lmerTest)       # p-values and tests for lme4 models
+library(maps)           # simple/base maps
+library(MASS)           # statistical methods (e.g., glm.nb)
+library(patchwork)      # combine and arrange plots
+library(performance)    # model checks and diagnostics
+library(rnaturalearth)  # spatial data for countries/continents
+library(scales)         # scales and formatting for visualizations
+library(sf)             # simple features for spatial data
+library(tidyverse)      # collection of data science packages
+library(viridis)        # perceptually uniform color palettes
